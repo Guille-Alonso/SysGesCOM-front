b1c4ae1bfc3cd59ebf9327a5364758d1c012c657
Tobias Alvarez <tosal1099@gmail.com>
2023-04-26 20:16:42 -0300

    agrego componente cambiar contraseña, falta funcion enviarDatos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/common/Layout/Layout";
 import HomePage from "./pages/HomePage";
+import CambiarContraseña from "./components/CambiarContraseña/CambiarContraseña";
 
 function App() {
   return (
     <Router>
       {/* <Layout> */}
         <Routes>
+        <Route path="/cambiar-contraseña" element={<CambiarContraseña />} />
           <Route path="/*" element={<HomePage />} />
         </Routes>
       {/* </Layout> */}
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,9 @@ export const LOGIN_VALUES = {
     email: "",
     password: "",
   };
+
+  export const CHANGE_PASSWORD_VALUES = {
+    password: "",
+    confirmPassword: "",
+    confirmPasswordRepeat: "",
+  };

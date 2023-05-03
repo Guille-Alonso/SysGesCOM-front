e375baee943582f7c22d36309ed1516bc9a19502
Walter Conticello <walterconticellomsmt@gmail.com>
2023-05-03 16:28:57 -0300

    correciones de errores menores y reformateo del footer

--- src/components/common/Layout/Footer/Footer.jsx
+++ src/components/common/Layout/Footer/Footer.jsx
@@ -5,51 +5,6 @@ const Footer = () => {
 	return (
 		<footer className="footer1">
 			<div className="movFooter">
-				<div className="waves">
-					<div className="wave" id="wave1"></div>
-					<div className="wave" id="wave2"></div>
-					<div className="wave" id="wave3"></div>
-					<div className="wave" id="wave4"></div>
-				</div>
-				<ul className="socialIcon">
-					<li>
-						<a href="">
-							<ion-icon name="logo-facebook"></ion-icon>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<ion-icon name="logo-twitter"></ion-icon>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<ion-icon name="logo-twitch"></ion-icon>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<ion-icon name="logo-facebook"></ion-icon>
-						</a>
-					</li>
-				</ul>
-				<ul className="menuFooter">
-					<li>
-						<a href="">Inicio</a>
-					</li>
-					<li>
-						<a href="">Nosotros</a>
-					</li>
-					<li>
-						<a href="">Servicios</a>
-					</li>
-					<li>
-						<a href="">Contacto</a>
-					</li>
-					<li>
-						<a href="">Equipo</a>
-					</li>
-				</ul>
 				<p>©2023 Equipo de Desarrollo COM</p>
 			</div>
 		</footer>

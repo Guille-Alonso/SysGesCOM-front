29328277e3e08dd1550c438ca64a32c94e8f2d22
Pedro Araoz <pedroaraozc@gmail.com>
2023-05-16 22:07:13 -0300

    Se agregó boton y columna de editar con su modal qeu se despliega, falta que llame los datos de la fila a editar y los plasme en el modal

--- src/components/common/Table/GeneralTable.jsx
+++ src/components/common/Table/GeneralTable.jsx
@@ -1,44 +1,128 @@
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
-import { Table } from "react-bootstrap";
-import './GeneralTable.css'
+import { FaEdit } from 'react-icons/fa'; // Importa el ícono de edición de React Icons
+import './GeneralTable.css';
+import { Table, Button, Modal } from "react-bootstrap";
 
-const GeneralTable = ({headings, items, setSelected, selected}) => {
 
-  return ( 
-    <MDBTable responsive className="generalTable">
-      <MDBTableHead  className="colorTabla">
-   
-      <tr>
-        { 
-        headings.map((heading)=><th scope="col" key={nanoid()}>{heading}</th>)
-        }        
-      </tr>
-      </MDBTableHead>
-      <MDBTableBody className="colorTabla">
-      {
-        
-        items.length!==0 && items.map((item)=>
-       
-        <tr key={nanoid()} onClick={()=>setSelected(item._id)} className={selected===item._id? 'row-selected' : ''}>
-          {
-    
-        Object.entries(item).map((elemento)=>{
-          if(elemento[0]!=='_id'){
-            return <td key={nanoid()}>{elemento[1]}</td>
-          }
-        })
+const EditModal = ({ show, onHide, onSave, editData, setSelectedItem, setShowModal }) => {
+  
+  const [editedData, setEditedData] = useState(editData);
 
-          }
-      
-        </tr>
-          )
-      }
-      
-      </MDBTableBody>
-    </MDBTable>
-   );
-}
- 
+  useEffect(() => {
+    setEditedData(editData);
+  }, [editData]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setEditedData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSave = () => {
+    onSave(editedData);
+    setShowModal(false);
+  };
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>Editar elemento</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {/* Aquí puedes agregar los campos de edición */}
+        <input
+          type="text"
+          name="columna1"
+          value={editedData ? editedData.columna1 : ""}
+          onChange={handleChange}
+        />
+        {/* Agrega más campos de edición según tus necesidades */}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Cerrar
+        </Button>
+        <Button variant="primary" onClick={handleSave}>
+          Guardar cambios
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+const GeneralTable = ({ headings, items, setSelected, selected }) => {
+
+  const [showModal, setShowModal] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleEdit = (itemId) => {
+    console.log("Edit item with ID:", itemId);
+    const itemToEdit = items.find((item) => item._id === itemId);
+    setSelectedItem(itemToEdit);
+    setShowModal(true);
+  };
+  
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSaveChanges = (editedData) => {
+    // Realiza las acciones necesarias para guardar los cambios
+    console.log("Save changes:", editedData);
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <MDBTable responsive className="generalTable">
+        <MDBTableHead className="colorTabla">
+          <tr>
+            {headings.map((heading) => (
+              <th scope="col" key={nanoid()}>
+                {heading}
+              </th>
+            ))}
+            <th scope="col">Editar</th>
+          </tr>
+        </MDBTableHead>
+        <MDBTableBody className="colorTabla">
+          {items.length !== 0 &&
+            items.map((item) => (
+              <tr
+                key={nanoid()}
+                onClick={() => setSelected(item._id)}
+                className={selected === item._id ? "row-selected" : ""}
+              >
+                {Object.entries(item).map((elemento) => {
+                  if (elemento[0] !== "_id") {
+                    return <td key={nanoid()}>{elemento[1]}</td>;
+                  }
+                  return null;
+                })}
+                <td>
+                  <FaEdit
+                    className="botonEditar"
+                    onClick={() => handleEdit(item._id)}
+                  />
+                </td>
+              </tr>
+            ))}
+        </MDBTableBody>
+      </MDBTable>
+      <EditModal
+        show={showModal}
+        onHide={handleCloseModal}
+        onSave={handleSaveChanges}
+        editData={selectedItem}
+        handleCloseModal={handleCloseModal}
+        handleSaveChanges={handleSaveChanges}
+      />
+    </>
+  );
+};
+
 export default GeneralTable;

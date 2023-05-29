ac3443a537555a446c8a272249e1c97f0835b728
guille alonso <guilloalonsot@gmail.com>
2023-05-29 10:12:38 -0300

    form y validaciones para editar camaras

---
+++ src/components/EditarCamaras/EditarCamaras.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect } from 'react'
+import { Alert, Button, Form } from 'react-bootstrap'
+import { EDITAR_CAMARA_VALUES } from '../../constants';
+import { validationsEditarCamara } from '../../helpers/validationsEditarCamara';
+import useForm from '../../hooks/useForm';
+
+const EditarCamaras = ({ onClose, getCamaras, camara }) => {
+
+    const editarCamara = async ()=>{
+        console.log(values);
+        // getCamaras();
+        // onClose();
+    }
+
+    const { handleChange, handleSubmit, values, setValues, errors } = useForm(
+        EDITAR_CAMARA_VALUES,
+        editarCamara,
+        validationsEditarCamara
+      );
+
+      useEffect(() => {
+        console.log(camara);
+        setValues(camara);
+      }, []);
+
+  return (
+    <>
+    <Form onSubmit={handleSubmit}>
+      <Form.Label>Cámara</Form.Label>
+      <Form.Control
+        className="inputAltaDeCamara"
+        type="text"
+        placeholder="Cámara"
+        value={values.nombre}
+        name="nombre"
+        onChange={handleChange}
+        required
+        maxLength={7}
+        minLength={6}
+      />
+      <Form.Label className="mt-4">Tipo de Cámara</Form.Label>
+      <Form.Select
+        onChange={handleChange}
+        className="inputAltaDeCamara"
+        name="tipo"
+        value={values.tipo}
+        required
+      >
+        <option value="">Seleccione una opción</option>
+        <option>camara</option>
+        <option>domo</option>
+      </Form.Select>
+      <Form.Label className="mt-4">Ubicación</Form.Label>
+      <Form.Control
+        className="inputAltaDeCamara"
+        type="text"
+        placeholder="Ubicación"
+        value={values.ubicacion}
+        name="ubicacion"
+        onChange={handleChange}
+        required
+        maxLength={30}
+        minLength={8}
+      />
+
+      <Button color="warning" shadow auto flat type="submit">
+        Editar
+      </Button>
+    </Form>
+      <div className="alertaError">
+      {Object.keys(errors).length !== 0 &&
+        Object.values(errors).map((error, index) => (
+          <Alert variant="danger" key={index}>
+            {error}
+          </Alert>
+        ))}
+    </div>
+    </>
+  );
+}
+
+export default EditarCamaras
---
+++ src/helpers/validationsEditarCamara.js
@@ -0,0 +1,24 @@
+export const validationsEditarCamara = (values) => {
+
+    let errors = {};
+  
+    if (!values.nombre) {
+      errors.nombre = "Campo obligatorio";
+    } else if (values.nombre.length < 6) {
+      errors.nombre = "El nombre de la cámara debe tener como mínimo 6 caracteres";
+    } else if (values.nombre.length > 7) {
+      errors.nombre = "El nombre de la cámara no debe poseer más de 7 caracteres";
+    } else if (!/^[a-zA-Z0-9 ]+$/.test(values.nombre)) {
+      errors.nombre = "El nombre no debe tener simbolos especiales";
+    }
+    if (!values.ubicacion) {
+      errors.ubicacion = "La ubicacion es obligatoria";
+    } else if (!/^[a-zA-Z0-9 ]+$/.test(values.ubicacion)) {
+      errors.ubicacion = "La ubicación no debe tener simbolos especiales";
+    }
+  
+    if (!values.tipo) {
+      errors.tipo = "Tipo de Camara obligatorio";
+    }
+    return errors;
+  };

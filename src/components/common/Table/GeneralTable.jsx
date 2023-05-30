bed0e1593467002f03675ccd0fa9d76c8bcf9b56
Pedro Araoz <pedroaraozc@gmail.com>
2023-05-30 20:38:32 -0300

    General Table creada Nuevamente en el component common

---
+++ src/components/common/Table/GeneralTable.jsx
@@ -0,0 +1,44 @@
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+import { Table } from "react-bootstrap";
+import './GeneralTable.css'
+
+const GeneralTable = ({headings, items, setSelected, selected}) => {
+
+  return ( 
+    <MDBTable responsive className="generalTable">
+      <MDBTableHead  className="colorTabla">
+   
+      <tr>
+        { 
+        headings.map((heading)=><th scope="col" key={nanoid()}>{heading}</th>)
+        }        
+      </tr>
+      </MDBTableHead>
+      <MDBTableBody className="colorTabla">
+      {
+        
+        items.length!==0 && items.map((item)=>
+       
+        <tr key={nanoid()} onClick={()=>setSelected(item._id)} className={selected===item._id? 'row-selected' : ''}>
+          {
+    
+        Object.entries(item).map((elemento)=>{
+          if(elemento[0]!=='_id'){
+            return <td key={nanoid()}>{elemento[1]}</td>
+          }
+        })
+
+          }
+      
+        </tr>
+          )
+      }
+      
+      </MDBTableBody>
+    </MDBTable>
+   );
+}
+ 
+export default GeneralTable;

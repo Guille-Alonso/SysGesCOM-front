5b2aeabd6fbb75f6d99221d1c4db7f67fa0314ec
Pedro Araoz <pedroaraozc@gmail.com>
2023-06-14 20:22:02 -0300

    solucion de errores

--- src/components/TableCamaras/TableCamaras.jsx
+++ src/components/TableCamaras/TableCamaras.jsx
@@ -112,92 +112,94 @@ const TableCamaras = ({
 
   return (
     <>
-      <div className="tableCont">
+      <div className="pagCont">
 
-        <Modal
-          className="modal-borrarUsuario"
-          show={modalDelete}
-          onHide={() => setModalDelete(false)}
-          backdrop="static"
-          centered
-        >
-          <div className="fondoModal">
-            <Modal.Header closeButton>
-              <h4>Borrar Cámara</h4>
-            </Modal.Header>
-            <div className="mensajeConfirmacion">
-              Seguro que quieres borrar esta Cámara?
+        <div className="tableCont">
+
+          <Modal
+            className="modal-borrarUsuario"
+            show={modalDelete}
+            onHide={() => setModalDelete(false)}
+            backdrop="static"
+            centered
+          >
+            <div className="fondoModal">
+              <Modal.Header closeButton>
+                <h4>Borrar Cámara</h4>
+              </Modal.Header>
+              <div className="mensajeConfirmacion">
+                Seguro que quieres borrar esta Cámara?
+              </div>
+              <Button
+                className="btn-BorrarUsuario"
+                variant="danger"
+                onClick={handleRemove}
+              >
+                Confirmar
+              </Button>
             </div>
-            <Button
-              className="btn-BorrarUsuario"
-              variant="danger"
-              onClick={handleRemove}
-            >
-              Confirmar
-            </Button>
-          </div>
-        </Modal>
-        <MDBTable responsive className="camaraTable">
-          <MDBTableHead className="colorTablaCamaras">
-            <tr>
-              {headings.map((heading) => (
-                <th scope="col" key={nanoid()}>
-                  {heading}
-                </th>
-              ))}
-              <th scope="col"></th>
-            </tr>
-          </MDBTableHead>
-          <MDBTableBody responsive className="bodyTabla">
-            {currentItems.length !== 0 &&
-              currentItems.map((item) => (
-                <tr
-                  key={nanoid()}
-                  onClick={() => setSelected(item._id)}
-                // className={selected === item._id ? "row-(selected" : ""}
-                >
-                  {Object.entries(item)
-                    .slice(1, -1)
-                    .map((elemento) => {
-                      // if (elemento[0] !== "_id") {
-                      return <td key={nanoid()}>{elemento[1].toUpperCase()}</td>;
-                      // }
-                      // return null;
-                    })}
-                  <td>
-                    <FaEdit
-                      className="botonEditar"
-                      onClick={() => handleEdit(item._id)}
-                    />
-                    <FaTrashAlt
-                      className="botonEliminar"
-                      onClick={() => setModalDelete(true)}
-                    />
-                  </td>
-                </tr>
-              ))}
-          </MDBTableBody>
-        </MDBTable>
-        <EditModal
-          show={showModal}
-          onHide={handleCloseModal}
-          onSave={handleSaveChanges}
-          editData={selectedItem}
-          handleCloseModal={handleCloseModal}
-          handleSaveChanges={handleSaveChanges}
-          editForm={
-            <EditarCamaras
-              onClose={handleCloseModal}
-              getCamaras={getCamaras}
-              camara={selectedItem}
-              className="boton-editar"
-            />
-          }
-        />
-      </div>
+          </Modal>
+          <MDBTable responsive className="camaraTable">
+            <MDBTableHead className="colorTablaCamaras">
+              <tr>
+                {headings.map((heading) => (
+                  <th scope="col" key={nanoid()}>
+                    {heading}
+                  </th>
+                ))}
+                <th scope="col"></th>
+              </tr>
+            </MDBTableHead>
+            <MDBTableBody responsive className="bodyTabla">
+              {currentItems.length !== 0 &&
+                currentItems.map((item) => (
+                  <tr
+                    key={nanoid()}
+                    onClick={() => setSelected(item._id)}
+                  // className={selected === item._id ? "row-(selected" : ""}
+                  >
+                    {Object.entries(item)
+                      .slice(1, -1)
+                      .map((elemento) => {
+                        // if (elemento[0] !== "_id") {
+                        return <td key={nanoid()}>{elemento[1].toUpperCase()}</td>;
+                        // }
+                        // return null;
+                      })}
+                    <td>
+                      <FaEdit
+                        className="botonEditar"
+                        onClick={() => handleEdit(item._id)}
+                      />
+                      <FaTrashAlt
+                        className="botonEliminar"
+                        onClick={() => setModalDelete(true)}
+                      />
+                    </td>
+                  </tr>
+                ))}
+            </MDBTableBody>
+          </MDBTable>
+          <EditModal
+            show={showModal}
+            onHide={handleCloseModal}
+            onSave={handleSaveChanges}
+            editData={selectedItem}
+            handleCloseModal={handleCloseModal}
+            handleSaveChanges={handleSaveChanges}
+            editForm={
+              <EditarCamaras
+                onClose={handleCloseModal}
+                getCamaras={getCamaras}
+                camara={selectedItem}
+                className="boton-editar"
+              />
+            }
+          />
+        </div>
 
 
-      <div className="paginacionCont">
+        <div className="paginacionCont">
           <Button
             className="paginacionBtnPrev"
             disabled={paginacion === 1}
@@ -215,6 +217,7 @@ const TableCamaras = ({
           >
             Siguiente
           </Button>
+        </div>
       </div>
     </>
   );

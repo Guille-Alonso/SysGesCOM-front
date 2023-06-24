f41974fc4d01383a635e6376cff3ae200d6d0d46
Tobias Alvarez <tosal1099@gmail.com>
2023-06-23 21:25:28 -0300

    modificando layouts, falta ponerle scroll a las tablas

--- src/components/ListarEventos/DetalleEvento.jsx
+++ src/components/ListarEventos/DetalleEvento.jsx
@@ -1,49 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import axios from '../../config/axios';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "../../config/axios";
+import "./DetalleEvento.css";
 
 const DetalleEvento = () => {
-    const [imageUrl, setImageUrl] = useState("");
-    const location = useLocation();
-    const datos = location.state;
+  const [imageUrl, setImageUrl] = useState("");
+  const location = useLocation();
+  const datos = location.state;
 
-    const styles = {
-      height: "300px",
-    };
+  const styles = {
+    height: "300px",
+  };
 
-    const getImg = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:4000/reportes/listar/${datos.reporte._id}`,
-          {
-            responseType: "blob", // Especifica el tipo de respuesta como Blob
-          }
-        );
+  const getImg = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/reportes/listar/${datos.reporte._id}`,
+        {
+          responseType: "blob", // Especifica el tipo de respuesta como Blob
+        }
+      );
 
-        const blob = response.data;
-        const url = URL.createObjectURL(blob);
-        setImageUrl(url);
-      } catch (error) {
-        toast.error(error.response?.data.message || error.message);
-      }
-    };
+      const blob = response.data;
+      const url = URL.createObjectURL(blob);
+      setImageUrl(url);
+    } catch (error) {
+      toast.error(error.response?.data.message || error.message);
+    }
+  };
 
-    useEffect(() => {
-      getImg();
-    }, []);
+  useEffect(() => {
+    getImg();
+  }, []);
 
   return (
     <div className="layoutHeight">
-      <label>{datos.reporte.fecha}</label>
-      <br />
-      <label>{datos.reporte.naturaleza.nombre}</label>
-      <br />
-      <label>{datos.reporte.detalle}</label>
-      <br />
-      <img style={styles} src={imageUrl} alt="Imagen" />
+      <div className="d-flex flex-column container contenedorDetalleReporte">
+        <label className="mb-2">
+          <strong>Fecha:</strong> {datos.reporte.fecha}
+        </label>
+        <label className="mb-2">
+          <strong>Tipo: </strong>
+          {datos.reporte.naturaleza.nombre.toUpperCase()}
+        </label>
+        <label className="mb-2">
+          <strong>Subcategoria: </strong>
+          {datos.reporte.subcategoria.nombre}
+        </label>
+        <label className="mb-5">
+          <strong>Detalle: </strong>
+          {datos.reporte.detalle}
+        </label>
+        <img
+          className="fotoReporteDetalle"
+          style={styles}
+          src={imageUrl}
+          alt="Imagen"
+        />
+      </div>
     </div>
   );
-}
+};
 
-export default DetalleEvento
+export default DetalleEvento;
--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -3,11 +3,9 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer1">
     <div className="movFooter">
       <p>©2023 Equipo de Desarrollo COM</p>
     </div>
-    </footer>
   );
 };
 

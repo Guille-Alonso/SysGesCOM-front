497c881e4f275aa89cc61931a0eaecf6f85e8681
Guillermo Alonso <g.alonso@comm.smt.org>
2023-08-28 18:20:24 -0300

    arreglando bugs varios

--- src/helpers/ValidationsAltaEvento.js
+++ src/helpers/ValidationsAltaEvento.js
@@ -12,8 +12,9 @@ export const validationsAltaEvento = (values) => {
       errors.detalle = "El detalle no puede tener mas de 500 caracteres";
     } else if(values.detalle.length < 4){
       errors.detalle = "El detalle no puede tener menos de 4 caracteres";
-    } else if(!/^[a-zA-Z\u00C0-\u017F0-9,.\s()"':;/]+$/.test(values.detalle)){
-      errors.detalle = "El detalle no puede tener simbolos especiales";
+    } else if(!/^[a-zA-Z\u00C0-\u017F0-9,."':;/]+(?:\s[a-zA-Z\u00C0-\u017F0-9,."':;/]+)*$/
+    .test(values.detalle)){
+      errors.detalle = "No se permiten símbolos especiales o espacios innecesarios.";
     }
 
     if (!values.naturaleza) {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,22 @@ import CardCambios from "../components/CardCambio/CardCambios";
 
 const HomePage = () => {
   const [reportes, loading] = useGet("/reportes/podio", axios);
+  const [tipoPodio, setTipoPodio] = useState("general")
 
+  function obtenerPeriodoDelDiaConHora(fecha) {
+    const horaActual = fecha.getHours();
+
+    if (horaActual >= 7 && horaActual < 15) {
+        return 'mañana';
+    } else if (horaActual >= 15 && horaActual < 23) {
+        return 'tarde';
+    } else {
+        return 'noche';
+    }
+}
+
+  const [reportesTurno, loadingTurno] = useGet(`/reportes/podio/${obtenerPeriodoDelDiaConHora(new Date())}`, axios);
+  
   return (
     <div className="layoutHeight">
       <div className="d-flex justify-content-around">
@@ -18,13 +33,27 @@ const HomePage = () => {
             <img src={workingSvg} className="inProgress" alt="" />
           </div>
         </main>
-        <aside className="contenedorCambios gap-5">
+        <aside className="contenedorCambios">
+
+          <select className="selectPodio mt-2" onChange={(e) => setTipoPodio(e.target.value)} value={tipoPodio}>
+            <option value="general">General</option>
+            <option value="turno">Mi Turno</option>
+          </select>
+
           {loading ? (
-            <Spinner variant="light" />
-          ) : (
+            <Spinner className="mt-3" variant="light" />
+          ) : tipoPodio == "general" && (
             <LeaderboardReportes reportes={reportes} />
           )}
+
+          {
+           tipoPodio == "turno" &&
+              <LeaderboardReportes reportes={reportesTurno} />
+          }
+          <div className="mt-3">
           <CardCambios />
+          </div>
+       
         </aside>
       </div>
       <div className="d-flex justify-content-around mt-5">

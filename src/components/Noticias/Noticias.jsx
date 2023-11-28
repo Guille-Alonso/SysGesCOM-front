a9e64472eca97ad976981aec78a886cd38b035a8
Pedro Araoz <pedroaraozc@gmail.com>
2023-11-27 21:35:42 -0300

    arreglos en css de Noticias

--- src/components/Noticias/Noticias.jsx
+++ src/components/Noticias/Noticias.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import "./Noticias.css";
 
 const Noticias = () => {
+
+
     return (
         <div className="noticias">
             <span className='text-center text-light mt-4 mb-4'>Noticias</span>
             <div className='bodyNoticias'>
-                <div className="contenidoNoticias">
-                    <a className="mt-3 circular">🔵 "circular pingo 1.pdf"</a>
-                    <a className="mt-3 circular">🔵 "circular pingo 2.pdf"</a>
-                    <a className="mt-3 circular">🔵 "circular pingo 3.pdf"</a>
-                    <a className="mt-3 circular">🔵 "el picnate se la come"</a>
-                    <a className="mt-3 circular">🔵 "circular pingo 4.pdf"</a>
+                <div className="contenidoNoticias mt-1">
+                    <div>
+                        <a className="mt-3 circular">🔵 "circular pingo 1.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 2.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 3.pdf"</a>
+                        <a className="mt-3 circular">🔵 "el picnate se la come"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 4.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 5.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 6.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 7.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 8.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 9.pdf"</a>
+                        <a className="mt-3 circular">🔵 "circular pingo 10.pdf"</a>
+
+                    </div>
                 </div>
                 <div className="aside">
                     <div className="contenidoAside">

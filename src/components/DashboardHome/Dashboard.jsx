0840c78caf35e547aa6b4e05d2edb65bb5f8ca1c
Guillermo Alonso <g.alonso@comm.smt.org>
2023-12-14 20:43:37 -0300

    quitando comentarios y mas

--- src/components/DashboardHome/Dashboard.jsx
+++ src/components/DashboardHome/Dashboard.jsx
@@ -24,23 +24,6 @@ const Dashboard = () => {
       ? useGet("/reportes/totalesVisualizadorYSupervisor", axios)
       : [];
 
-  // function getRandomColor() {
-  //   const randomInt = (min, max) =>
-  //     Math.floor(Math.random() * (max - min + 1) + min);
-
-  //   const colors = [];
-  //   for (let i = 0; i < 16; i++) {
-  //     const red = randomInt(0, 255);
-  //     const green = randomInt(0, 255);
-  //     const blue = randomInt(0, 255);
-
-  //     const rgbaColor = `rgba(${red}, ${green}, ${blue}, 1)`;
-  //     colors.push(rgbaColor);
-  //   }
-
-  //   return colors;
-  // }
-
   const categoriasLabels = () => {
     let countObj = {}; // Objeto para almacenar la cantidad de reportes por categoría
     let cats = [];
